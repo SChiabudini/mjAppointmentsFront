32a3c6f8d6bf5ecdb1afbe9a25e62def051ee4db
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-01-13 18:44:33 -0300

    printable budget done

--- src/components/pages/Budget/Budgets.jsx
+++ src/components/pages/Budget/Budgets.jsx
@@ -81,9 +81,7 @@ const Budgets = () => {
         const options = { 
             day: '2-digit', 
             month: '2-digit', 
-            year: 'numeric', 
-            hour: '2-digit', 
-            minute: '2-digit', 
+            year: 'numeric',
             timeZone: 'UTC' 
         };
 
