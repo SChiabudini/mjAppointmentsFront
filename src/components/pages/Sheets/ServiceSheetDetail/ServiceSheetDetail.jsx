568b7b948b144927a1aad57211b2335ee9af35ea
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-01-29 22:29:34 -0300

    print sheets detail done

--- src/components/pages/Sheets/ServiceSheetDetail/ServiceSheetDetail.jsx
+++ src/components/pages/Sheets/ServiceSheetDetail/ServiceSheetDetail.jsx
@@ -82,108 +82,95 @@ const ServiceSheetDetail = () => {
                             </div>
                         </div>
                         <div id="printable-section" className={`${style.serviceSheet} ${!serviceSheetDetail.active ? 'disabled' : ''}`}>
-
-                            <div className={`columns ${!serviceSheetDetail.active ? 'disabled' : ''}`}>
-                                <div>
-                                    {serviceSheetDetail.number && <p><span>Número de ficha:&nbsp;</span>{serviceSheetDetail.number}</p>}
-                                    {serviceSheetDetail.date && <p><span>Fecha:&nbsp;</span></p>}
-                                    <p><span>{new Intl.DateTimeFormat('es-ES', { 
-                                        day: '2-digit', 
-                                        month: '2-digit', 
-                                        year: '2-digit', 
-                                        hour: '2-digit', 
-                                        minute: '2-digit', 
-                                        timeZone: 'UTC' // Forzar UTC
-                                    }).format(new Date(serviceSheetDetail.date))} hs</span></p>
-                                    {(serviceSheetDetail.personClient || serviceSheetDetail.companyClient) && (
-                                        <p><span>Cliente:&nbsp;</span></p>
-                                    )}                 
-                                    {serviceSheetDetail.personClient ? (
-                                        <div>
-                                            {serviceSheetDetail.personClient.name && <p><span>Nombre:&nbsp;</span>{serviceSheetDetail.personClient.name}</p>}
-                                            {serviceSheetDetail.personClient.dni && <p><span>DNI:&nbsp;</span>{serviceSheetDetail.personClient.dni}</p>}
-                                            {serviceSheetDetail.personClient.cuilCuit && <p><span>CUIL/CUIT:&nbsp;</span>{serviceSheetDetail.personClient.cuilCuit}</p>}
-                                            {serviceSheetDetail.personClient.email && <p><span>Correo electrónico:&nbsp;</span>{serviceSheetDetail.personClient.email}</p>}
-                                            {serviceSheetDetail.personClient.phoneWsp.numberPhone ? (
-                                                <li><span>Whatsapp:&nbsp;</span>+{serviceSheetDetail.personClient.phoneWsp.prefix}{serviceSheetDetail.personClient.phoneWsp.numberPhone}</li>
-
-                                            ) : (
-                                                <li><span>Whatsapp:&nbsp;</span>No Whatsapp registrado.</li>
-                                            )} 
-                                            {serviceSheetDetail.personClient.phones?.length > 0 ? (
-                                                <div>
-                                                    <li>
-                                                        <span>Teléfonos:&nbsp;</span>
-                                                        {serviceSheetDetail.personClient.phones?.join(', ')}
-                                                    </li>
-                                                </div>
-                                            ) : (
-                                                <li><span>Teléfonos:&nbsp;</span>No tiene teléfono registrado.</li>
-                                            )}
-                                        </div>
-                                    ) : (
-                                        <></>
-                                    )}      
-                                    {serviceSheetDetail.companyClient ? (
-                                        <div>
-                                            {serviceSheetDetail.companyClient.name && <p><span>Nombre:&nbsp;</span>{serviceSheetDetail.companyClient.name}</p>}
-                                            {serviceSheetDetail.companyClient.cuit && <p><span>CUIT:&nbsp;</span>{serviceSheetDetail.companyClient.cuit}</p>}
-                                            {serviceSheetDetail.companyClient.address && <p><span>Dirección:&nbsp;</span>{serviceSheetDetail.companyClient.address}</p>}
-                                            {serviceSheetDetail.companyClient.email && <p><span>Correo electrónico:&nbsp;</span>{serviceSheetDetail.companyClient.email}</p>}
-                                            {serviceSheetDetail.companyClient.phoneWsp.numberPhone ? (
-                                                <li><span>Whatsapp:&nbsp;</span>+{serviceSheetDetail.companyClient.phoneWsp.prefix}{serviceSheetDetail.companyClient.phoneWsp.numberPhone}</li>
-                                            ) : (
-                                                <li><span>Whatsapp:&nbsp;</span>No Whatsapp registrado.</li>
-                                            )} 
-                                            {serviceSheetDetail.companyClient.phones?.length > 0 ? (
-                                                <div>
-                                                    <li>
-                                                        <span>Teléfonos:&nbsp;</span>
-                                                        {serviceSheetDetail.companyClient.phones?.join(', ')}
-                                                    </li>
-                                                </div>
-                                            ) : (
-                                                <li><span>Teléfonos:&nbsp;</span>No tiene teléfono registrado.</li>
-                                            )}
-                                        </div>
-                                    ) : (
-                                        <></>
-                                    )} 
-                                    {serviceSheetDetail.vehicle && <p><span>Vehículo:&nbsp;</span></p>}
-                                    {serviceSheetDetail.vehicle ? (
-                                        <ul key={serviceSheetDetail.vehicle.licensePlate}>
-                                            <li> 
-                                                {serviceSheetDetail.vehicle.licensePlate && <p><span>Patente:&nbsp;</span>{serviceSheetDetail.vehicle.licensePlate}</p>}
-                                                {serviceSheetDetail.vehicle.brand && <p><span>Marca:&nbsp;</span>{serviceSheetDetail.vehicle.brand}</p>}
-                                                {serviceSheetDetail.vehicle.model && <p><span>Modelo:&nbsp;</span>{serviceSheetDetail.vehicle.model}</p>}
-                                                {serviceSheetDetail.vehicle.year && <p><span>Año:&nbsp;</span>{serviceSheetDetail.vehicle.year}</p>}
-                                                {serviceSheetDetail.vehicle.engine && <p><span>Motor:&nbsp;</span>{serviceSheetDetail.vehicle.engine}</p>}
-                                            </li>
-                                        </ul>
-                                    ) : (
-                                        <p>No hay vehículo registrado.</p>
-                                    )}
-                                    {serviceSheetDetail.kilometers && <p><span>Kilómetros:&nbsp;</span>{serviceSheetDetail.kilometers}</p>}
-                                    {serviceSheetDetail.kmsToNextService && <p><span>Kilómetros hasta el siguiente service:&nbsp;</span>{serviceSheetDetail.kmsToNextService}</p>}
-                                    {serviceSheetDetail.oil && <p><span>Aceite:&nbsp;</span>{serviceSheetDetail.oil}</p>}
-                                    {serviceSheetDetail.filters?.length > 0 ? (
-                                        <div>
-                                            <p><span>Filtros:&nbsp;</span></p>
-                                            {serviceSheetDetail.filters?.map((filter, index) => (
-                                                <ul key={index}>
-                                                    <li>
-                                                        {<p><span>{filter}</span></p>}
-                                                    </li>
-                                                </ul>
-                                            ))}
-                                        </div>
-                                    ) : (
-                                        <></>
-                                    )}
-                                    {serviceSheetDetail.notes && <p><span>Notas:&nbsp;</span>{serviceSheetDetail.notes}</p>}
-                                    {serviceSheetDetail.amount && <p><span>Monto:&nbsp;$&nbsp;</span>{serviceSheetDetail.amount}</p>}                                                   
+                            <div className={style.header}>
+                                <div className={style.header1}>
+                                    <img src={logo} alto=""/>
+                                    <p><span>MJ PRO OIL</span></p>
+                                    <p>Matías Nicolás Ezequiel</p>
+                                    <p>20-41195480-4</p>
+                                    <p>Av. 44 n° 1877 e/ 132 y 133</p>
+                                    <p>Teléfono: (221) 6230285</p>
+                                    <p>Email: mjprooil44@gmail.com</p>
+                                </div>
+                                <div className={style.header2}>
+                                    <p className={style.bigP}>P</p>
+                                    <p>DOCUMENTO</p>
+                                    <p>NO VÁLIDO</p>
+                                    <p>COMO FACTURA</p>
+                                </div>
+                                <div className={style.header3}>
+                                    <div>
+                                        <p><span>FICHA DE SERVICE</span></p>
+                                        <p>N° {serviceSheetDetail.number}</p>
+                                        <p>Fecha: {new Intl.DateTimeFormat('es-ES', { 
+                                                day: '2-digit', 
+                                                month: '2-digit', 
+                                                year: '2-digit', 
+                                                hour: '2-digit', 
+                                                minute: '2-digit', 
+                                                timeZone: 'UTC'
+                                        }).format(new Date(serviceSheetDetail.date))}hs</p>
+                                    </div>
                                 </div>
                             </div>
+                            <div className={style.body}>
+                                <div className={style.dataColumns}>
+                                    <div className={style.dataContainer}>
+                                        <p><span>DATOS DEL CLIENTE</span></p>
+                                        {serviceSheetDetail.personClient ? (
+                                            <div className={style.data}>
+                                                
+                                                    <p><span>DNI:</span> {serviceSheetDetail.personClient.dni}</p>
+                                                    <p><span>Nombre:</span> {serviceSheetDetail.personClient.name}</p>
+                                                    <p><span>Email:</span> {serviceSheetDetail.personClient.email}</p>
+                                                    {serviceSheetDetail.personClient.cuilCuit && <p><span>CUIL/CUIT:</span> {serviceSheetDetail.personClient.cuilCuit}</p>}
+                                                    {serviceSheetDetail.personClient.phoneWsp.numberPhone && <p><span>Whatsapp:&nbsp;</span>+{serviceSheetDetail.personClient.phoneWsp.prefix}{serviceSheetDetail.personClient.phoneWsp.numberPhone}</p>}
+                                                    {serviceSheetDetail.personClient.phones?.length > 0 && <p><span>Teléfonos:</span> {serviceSheetDetail.personClient.phones?.join(', ')}</p>}
+                                                
+                                            </div>
+                                        ) : (
+                                            <div className={style.data}>
+                                                                                        
+                                                <p><span>CUIT:</span> {serviceSheetDetail.companyClient.cuit}</p>
+                                                <p><span>Nombre:</span> {serviceSheetDetail.companyClient.name}</p>
+                                                <p><span>Email:</span> {serviceSheetDetail.companyClient.email}</p>
+                                                {serviceSheetDetail.companyClient.address && <p><span>Dirección:</span> {serviceSheetDetail.companyClient.address}</p>}
+                                                {serviceSheetDetail.companyClient.phoneWsp.numberPhone && <p><span>Whatsapp:&nbsp;</span>+{serviceSheetDetail.companyClient.phoneWsp.prefix}{serviceSheetDetail.companyClient.phoneWsp.numberPhone}</p>}
+                                                {serviceSheetDetail.companyClient.phones?.length > 0 && <p><span>Teléfonos:</span> {serviceSheetDetail.companyClient.phones?.join(', ')}</p>}
+
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className={style.dataContainer}>
+                                        <p><span>DATOS DEL VEHÍCULO</span></p>
+                                        <div className={style.data}>                                                                                    
+                                            <p><span>Patente:</span> {serviceSheetDetail.vehicle.licensePlate}</p>
+                                            <p><span>Marca:</span> {serviceSheetDetail.vehicle.brand}</p>
+                                            <p><span>Modelo:</span> {serviceSheetDetail.vehicle.model}</p>
+                                            <p><span>Año:</span> {serviceSheetDetail.vehicle.year}</p>
+                                            <p><span>Motor:</span> {serviceSheetDetail.vehicle.engine}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={style.dataContainer}>
+                                    <p><span>DATOS DEL SERVICE</span></p>
+                                    <div className={style.data}>
+                                        {serviceSheetDetail.kilometers && <p><span>Kilómetros:&nbsp;</span>{serviceSheetDetail.kilometers}</p>}
+                                        {serviceSheetDetail.kmsToNextService && <p><span>Kilómetros hasta el siguiente service:&nbsp;</span>{serviceSheetDetail.kmsToNextService}</p>}
+                                        {serviceSheetDetail.oil && <p><span>Aceite:&nbsp;</span>{serviceSheetDetail.oil}</p>}
+                                        {serviceSheetDetail.filters?.length > 0 ? (
+                                            
+                                            <p><span>Filtros:&nbsp;</span>{serviceSheetDetail.filters?.join(', ')}</p>
+                                            
+                                        ) : (
+                                            <></>
+                                        )}
+                                        {serviceSheetDetail.notes && <p><span>Notas:&nbsp;</span></p>}
+                                        {serviceSheetDetail.notes && <p className={style.notes}>{serviceSheetDetail.notes}</p>}
+                                    </div>  
+                                </div>
+                            </div>
+                            <div className={style.footer}>Total: ${serviceSheetDetail.amount}</div>
                         </div>
                     </div>
 
