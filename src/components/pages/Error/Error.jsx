ca669ef27e128c8c8cc98fda8268e987da40b66c
Sofía Chiabudini <122496655+SChiabudini@users.noreply.github.com>
2025-01-30 21:19:46 -0300

    error page styles done

--- src/components/pages/Error/Error.jsx
+++ src/components/pages/Error/Error.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import error from '../../../assets/img/error.png';
 
 const Error = () => {
-  return (
-    <div>
-        <p>Error al cargar los datos del servidor. Por favor, reiniciar el servidor.</p>
-    </div>
-  )
+    return (
+        <div className='error'>
+            <div>
+                <img src={error} alt=""/>
+                <h2>Error al cargar los datos del servidor</h2>
+                <ul>
+                    <li>Chequear la conexión a internet del dispositivo.</li>
+                    <li>Reiniciar la aplicación.</li>
+                    <li>Si el problema persiste, contactarse con <span>Frida Software</span>.</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Error;
